d725d875c270d73a9eaeb92449c7954638e86ea3
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-04-28 10:58:20 +0800

    tested networkImg

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('No to Diabetes'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am diabetic'),
+          backgroundColor: Colors.red[700],
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuj2hThlzNTW4yspAlxRkZQljvWpA7pimAXCPqJ28HrTJh95A-&usqp=CAU'),
+          ),
+        ),
       ),
     ),
   );
