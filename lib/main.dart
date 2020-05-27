141b87335817ccc1953e7e849c7beeaf61396e14
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-05-27 15:39:52 +0800

    container added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-  DiabetesCardApp()
+  runApp(DiabetesCardApp());
 }
 
 class DiabetesCardApp extends StatelessWidget {
@@ -15,12 +14,17 @@ class DiabetesCardApp extends StatelessWidget {
           backgroundColor: Colors.red[700],
         ),
         backgroundColor: Colors.white,
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diabetesAwareness02.png'),
+        body: SafeArea(
+          child: Container(
+            height: 200,
+            width: 200,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('I am diabetic'),
           ),
         ),
       ),
-    ),;
+    );
   }
 }
